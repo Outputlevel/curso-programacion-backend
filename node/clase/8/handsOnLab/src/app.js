4e8bccf2a1a1f8984d89881b3e5a12004f19583d
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-07-31 21:29:46 -0300

    Add express static in class 8

--- node/clase/8/handsOnLab/src/app.js
+++ node/clase/8/handsOnLab/src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 //Use routers
 app.use('/api/users', routerUsers);
