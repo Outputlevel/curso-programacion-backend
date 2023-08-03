ca05c4d8010720856740cf95b2105e5e3902f5d4
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-08-02 21:19:34 -0300

    remove route folder in ejemplo-handlebars

--- node/clase/9/ejemplo-handlebars/src/app.js
+++ node/clase/9/ejemplo-handlebars/src/app.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-//import routerUsers from './routes/users.js';
 import __dirname from './utils.js';
 
 const app = express();
-
-//app.use(express.json());
-//app.use(express.urlencoded({extended: true}));
 
 //Se inicializa el motor de plantillas
 app.engine('handlebars', handlebars.engine());
@@ -19,9 +15,6 @@ app.set('view engine', 'handlebars');
 
 //Establece el servidor estático de archivos
 app.use(express.static('public'));
-
-//Utiliza las rutas definidas
-//app.use('/users', routerUsers);
 
 app.get('/', (req, res) => {
     const name = req.query.name ?? "Usuario";
