053e424a33a6849f4a78d6b40b3b7d32fbcfffda
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-08-02 22:41:39 -0300

    Add actividad-2 in class 9

---
+++ node/clase/9/actividad-2/src/routes/users.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { users } from "../app.js";
+
+const router = Router();
+
+router.post("/", (req, res) => {
+    const { name, email, password } = req.body;
+    users.push({ name, email, password });
+    res.redirect('/'); //Redirige al home una vez realizada la accion
+});
+
+export default router;
