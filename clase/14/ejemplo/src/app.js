96e7df21f55fd463a66d0605a6387b4acca9d51b
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-08-23 21:57:19 -0300

    Added class 14

---
+++ clase/14/ejemplo/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import routerUsers from './routes/userRouter.js';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//const uri = 'mongodb://127.0.0.1:27017/class-14';
+//Replace <user> and <password>
+const uri = 'mongodb+srv://<user>:<password>@cluster0.usycu2f.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(uri);
+
+//Use routers
+app.use('/api/users', routerUsers);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Start Server in Port ${PORT}`);
+});
---
+++ clase/14/ejemplo/src/routes/userRouter.js
@@ -0,0 +1,94 @@
+import { Router } from "express";
+import { userModel } from "../models/userModel.js";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await userModel.find();
+        res.send({
+            status: 'success',
+            payload: users
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({
+            status: 'error',
+            message: error.message
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+
+    const {firstName, lastName, email} = req.body;
+
+    if (!firstName || !lastName || !email) {
+        return res.status(400).send({
+            status: 'error',
+            message: 'Incomplete values'
+        });
+    }
+
+    const result = await userModel.create({
+        firstName,
+        lastName,
+        email
+    });
+
+    res.status(201).send({
+        status: 'success',
+        payload: result
+    });
+});
+
+router.put('/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const {firstName, lastName, email} = req.body;
+
+    if (!firstName || !lastName || !email) {
+        return res.status(400).send({
+            status: 'error',
+            message: 'Incomplete values'
+        });
+    }
+
+    try {
+        const result = await userModel.updateOne({_id: id}, {
+            firstName,
+            lastName,
+            email
+        });
+    
+        res.status(200).send({
+            status: 'success',
+            payload: result
+        });
+    } catch (error) {
+        res.status(400).send({
+            status: 'error',
+            message: error.message.replace(/"/g, "'")
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    try {
+        const result = await userModel.deleteOne({_id: id});
+        res.status(200).send({
+            status: 'success',
+            payload: result
+        });
+    } catch (error) {
+        res.status(400).send({
+            status: 'error',
+            message: error.message.replace(/"/g, "'")
+        });
+    }
+});
+
+export default router;
